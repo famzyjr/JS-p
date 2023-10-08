9bafe699df0ef9ccdb316b8fc8f19830bb801503
famzyjr <famuyiwaahmed132@gmail.com>
2023-10-08 02:12:07 +0100

    learning js

---
+++ js.js
@@ -0,0 +1,32 @@
+//strings//
+let email = 'famuiwaahmed132@gmail.com';
+console.log(email);
+
+//string concatination//
+let firstName = 'ahmed';
+let  lastName =  'Famuyiwa';
+let  resultName =  'lazy';
+
+
+let fullName = firstName + " "+ lastName + " " + resultName;
+console.log(fullName);
+
+
+// getting characters//
+console.log(fullName[5]);
+
+ // string length//
+  console.log(fullName.length);
+
+  //string methods//
+  console.log(lastName.toUpperCase());
+
+   let index = email.indexOf(1);
+     console.log(index);
+
+
+
+
+
+
+
---
+++ m.js
@@ -0,0 +1,31 @@
+// //  let firstName = ("Famuyiwa");
+//      age   = 12;
+//      Class = ("ss3");
+
+//      console.log()
+
+// concatination
+let firstName = ("Famuyiwa");
+      Status = ("single")
+     Class = ("Ss3");
+    
+     let person = firstName + " " + Status + " " + Class ;
+     console.log(person)
+  // getting a string character//
+     console.log(person[18]);
+ 
+     // string length//
+
+     console.log(person.length)
+
+     // string methods//
+
+     console.log(person.toUpperCase())
+
+     console.log(person.slice(1,4))
+
+    
+
+     console.log(person.replace(3, 4))
+
+
